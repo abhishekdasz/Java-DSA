c2cd6db764a105f6adf0ceb1284b2bb627f45758
Abhishek Das <abhishekdaszy@gmail.com>
2026-05-01 22:06:46 +0530

    Mode of Array

--- 1Arrays/ArrayPractice.java
+++ 1Arrays/ArrayPractice.java
@@ -18,11 +18,38 @@ public class ArrayPractice
             System.out.println("Num:"+ i + " Freq:"+ freq.get(i));
         }
     }
+    void modeOfAnArray(int arr[])
+    {
+        Map <Integer, Integer> map = new HashMap<>();
+        for(int num: arr)
+        {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        System.out.println(map);
+
+        int maxFreqCount = 0;
+        int maxFreqCountKey = 0;
+        for(int num: map.keySet())
+        {
+            int currentKey = num;
+            int freqOfCurrentkey = map.get(num);
+            if(freqOfCurrentkey>maxFreqCount)
+            {
+                maxFreqCount = freqOfCurrentkey;
+                maxFreqCountKey = currentKey;
+            }
+        }
+        System.out.println("Max Freq Key: "+maxFreqCountKey+ " count: "+maxFreqCount);
+    }
     public static void main(String[] args) 
     {
         int arr[] = {1,2,2,5,2,2,4,1,4,3,3,4};
         ArrayPractice obj = new ArrayPractice();
         System.out.println("Freq of Num in an array:");
         obj.freqOfNumInArray(arr);
+
+        int arr2[] = {1,1,1,1,2,2,2,3,3,4};
+        System.out.println("Mode of Array:");
+        obj.modeOfAnArray(arr2);
     }
 }
