1aea4755ed51284d679222c5311a75397ce502d6
Abhishek Das <abhishekdaszy@gmail.com>
2026-05-04 21:44:58 +0530

    obj.z(arr);

--- 4ArrayManipulation/SortZeroOne.java
+++ 4ArrayManipulation/SortZeroOne.java
@@ -33,10 +33,42 @@ public class SortZeroOne
             System.out.print(sortedArr[i]+" ");
         }
     }
+    void sortArrayTwoPointer(int arr[])
+    {
+        int left = 0;
+        int right = arr.length-1;
+        while(left<right)
+        {
+            if(arr[left] == 1 && arr[right] == 0)
+            {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            else if(arr[left] == 0)
+            {
+                left++;
+            }
+            else if(arr[right] == 1)
+            {
+                right--;
+            }
+        }
+        System.out.println();
+        System.out.println("Printing sorted 0s & 1s:");
+        for(int i=0; i<=arr.length-1; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
     public static void main(String[] args) 
     {
         int arr[] = {1,0,1,1,0,0,1};
         SortZeroOne obj = new SortZeroOne();
         obj.sortArray(arr);
+
+        obj.sortArrayTwoPointer(arr);
     }
 }
