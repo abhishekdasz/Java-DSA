31ff74a36ebf71a1d79e82c275bab25c1b6652a0
Abhishek Das <abhishekdaszy@gmail.com>
2026-05-05 23:46:42 +0530

    bitwise

---
+++ 5BitwiseOperations/Bitwise.java
@@ -0,0 +1,12 @@
+public class Bitwise 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(1&1);
+        System.out.println(2&2);
+        System.out.println(1^1);
+        System.out.println(2^3);
+        System.out.println(4^1);
+
+    }
+}
