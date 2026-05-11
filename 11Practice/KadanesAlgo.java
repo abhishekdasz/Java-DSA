1ab0b07ce065b06af4da5d374fea4c5830cd40f6
Abhishek Das <abhishekdaszy@gmail.com>
2026-05-12 00:42:08 +0530

    kadanes algorithm

---
+++ 11Practice/KadanesAlgo.java
@@ -0,0 +1,27 @@
+public class KadanesAlgo 
+{
+    void kadaneAlgo(int arr[])
+    {
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<=arr.length-1; i++)
+        {
+            sum = sum+arr[i];
+            if(sum<0)
+            {
+                sum = 0;
+            }
+            if(sum>maxSum)
+            {
+                maxSum = sum;
+            }
+        }
+        System.out.println(maxSum);
+    }
+    public static void main(String[] args) 
+    {
+        int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        KadanesAlgo obj = new KadanesAlgo();
+        obj.kadaneAlgo(arr);
+    }    
+}
--- 11Practice/tempCodeRunnerFile.java
+++ 11Practice/tempCodeRunnerFile.java
@@ -1,1 +1,8 @@
-arr
+        // for(int i=0; i<=arr.length-1; i++)
+        // {
+        //     sum = sum+arr[0];
+        //     // if(sum<0)
+        //     // {
+        //     //     sum = 0;
+        //     // }
+        // }
