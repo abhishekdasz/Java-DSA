d25e3a58793b2b391cacb3442bf5107ecd677310
Abhishek Das <abhishekdaszy@gmail.com>
2026-05-14 01:27:34 +0530

    transpose of matrix

---
+++ 13Practice/TransposeOfMatrix.java
@@ -0,0 +1,15 @@
+public class TransposeOfMatrix 
+{
+    public static void main(String[] args) 
+    {
+        int arr[][] = {{1,2,3},{4,5,6}};
+        for(int i=0; i<=arr[0].length-1; i++)
+        {
+            for(int j=0; j<=arr.length-1; j++)
+            {
+                System.out.print(arr[j][i] + " ");
+            } 
+            System.out.println();
+        }
+    }
+}
