9f096e2da2324a37cce3169763e87186f7af4c3c
Abhishek Das <abhishekdaszy@gmail.com>
2026-05-21 23:45:39 +0530

    insertion sort

---
+++ 21Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+class InsertionSort
+{
+    public static void main(String[]args)
+    {
+        int arr[]={5,1,6,2,4,3};
+
+        for(int i=1; i<arr.length; i++)
+        {
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && arr[prev]>curr)
+            {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
